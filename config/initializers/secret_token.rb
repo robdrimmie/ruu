f196bf218d9fc9ac593011904d8c65a7f6b9ccbf
rmdrimmie <rob.drimmie@gmail.com>
2011-02-04 11:14:17 -0500

    Are You You?
    My first ever ruby app, maybe a bit ambitious. Asks for a XMPP-referencable account, sends a message via XMPP and if approved by that account holder, the web visitor gets in.
    
    Very prone to badness, but fun and silly. I hope.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ruu::Application.config.secret_token = '9db9677250a6752a511b628823a60296552d0b99229ab18408559ce67afc1110ff4bb79ca4154b0c3fdcc20b365be7c14e5f29d1b8928c1ca76bc89a8de06c59'
