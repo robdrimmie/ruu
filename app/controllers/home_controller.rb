62a8e1110d09894f288548cebfe8602d23917191
rmdrimmie <rob.drimmie@gmail.com>
2011-02-04 21:01:54 -0500

    Ok, I figured out how to get XMPP working. The problem is the process needs to take the form: - check roster to see if user is on it - if not:  - request communication privileges  - grant communication privileges - in all cases:  - send "Are You You" message  - process response  - display "You ARE you" or "You AREN'T you" message.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,25 @@
+require 'xmpp4r/client'
+include Jabber
+
 class HomeController < ApplicationController
-  def index
-  end
+ def index
+  #Jabber::debug = true # Uncomment this if you want to see what's being sent and received!
+  jid = JID::new('rob.drimmie@gmail.com')
+  password = 'fake';
+logger.debug( "foo" );  
+  cl = Client::new(jid)
+  cl.connect
+  cl.auth(password)
+
+  to = "rob.drimmie@gmail.com"
+  subject = "XMPP4R test"
+  body = "Hi, this is my first try from XMPP4R!!!"
+  m = Message::new(to, body).set_type(:chat).set_id('1').set_subject(subject)
+  cl.send m
+
+  return render :text => "The object is #{cl}"
+
+
+ end
 
 end
